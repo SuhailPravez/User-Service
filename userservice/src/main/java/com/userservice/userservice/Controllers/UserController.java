3957158c379d8f265f5079d1f8021234a7e527de
Suhail Ahamad <140061255+SuhailPravez@users.noreply.github.com>
2025-08-06 23:43:25 +0530

    minor

--- userservice/src/main/java/com/userservice/userservice/Controllers/UserController.java
+++ userservice/src/main/java/com/userservice/userservice/Controllers/UserController.java
@@ -17,9 +17,9 @@ import org.springframework.web.bind.annotation.RestController;
 import com.userservice.userservice.Entities.User;
 import com.userservice.userservice.Services.UserService;
 
-import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+// import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
-import io.github.resilience4j.retry.annotation.Retry;
+// import io.github.resilience4j.retry.annotation.Retry;
 
 @RestController
 @RequestMapping("/user")
